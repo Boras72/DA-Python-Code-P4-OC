728acb1b0ce0263cf0a4c278865510126eaeb820
Boras72D <borasomiko@gmail.com>
2024-05-03 16:23:04 +0200

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#main.py (fichier principal pour initialisez et lancez l'application) connecté au MainController qui est le moteur, le main d'oeuvre de ce programme alors que main.py n'est que le déclencheur
+#main.py (fichier principal pour initialiser et lancer l'application) connecté au MainController qui est le moteur, le main d'oeuvre, le mécanisme principal de ce programme alors que main.py n'est que le déclencheur
 
 from Controllers.MenuController import MainController
-#importer la classe [from nom_dossier.nom_fichier import nom_class]
+#importer la classe => [from nom_dossier.nom_fichier import nom_class]
 
 if __name__ == '__main__':
     MainController().start_menu()   
